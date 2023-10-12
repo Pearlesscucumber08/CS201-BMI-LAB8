773d460303d91e5edc114bd842ef16d50673d350
Brigitte Mae Ibeng <115964613+Pearlesscucumber08@users.noreply.github.com>
2023-10-12 18:51:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let firstName = "Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB8");
+
+const favSingers = ["BTS", "Maroon5", "FmSTATIC"];
+console.log (favSingers);
+const favNumbers = ["16","8","9","4"];
+console.log (favNumbers);
+const mixedArr = ["string",["otherarray"],123,true];
+
+console.log (favSingers[0]);
+console.log (favSingers[1]);
+console.log (favSingers[2]);
+console.log (favNumbers[0]);
+console.log (favNumbers[1]);
+console.log (favNumbers[2]);
+console.log (favNumbers[3]);
+console.log (mixedArr[0]);
+console.log (mixedArr[1]);
+console.log (mixedArr[2]);
+console.log (mixedArr[3]);
+
+
+//   // If you want your users to input the nagyan of your var you can use this code
+// var favSingers = [];
+// for (let i = 0; i < 3; i++){
+//     var userInput = prompt ("Enter three of your most favorite singers");
+//   // use the enter button when you want to type the next artist
+//     favSingers.push(userInput);
+// }
+// console.log (favSingers);
+
+// var favNumbers = [];
+// for (let i = 0; i < 4; i++){
+//     var favNumber = prompt ("Enter four of your most favorite numbers");
+//   // use the enter button when you want to type the next artist
+//     favNumbers.push(favNumber);
+// }
+// console.log (favNumbers);
+
+// const mixedArr = ["string",["otherarray"],123,true];
+
+// console.log(favSingers[0]);
+// console.log(favSingers[1]);
+// console.log(favSingers[2]);
+// console.log(favNumbers[0]);
+// console.log(favNumbers[1]);
+// console.log(favNumbers[2]);
+// console.log(favNumbers[3]);
+// console.log (mixedArr[0]);
+// console.log (mixedArr[1]);
+// console.log (mixedArr[2]);
+// console.log (mixedArr[3]);
